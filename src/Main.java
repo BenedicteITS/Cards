a8ed26be31631296cddb3c2a832b9dc7b4eee366
BenedicteITS <122403437+BenedicteITS@users.noreply.github.com>
2025-07-23 17:43:10 +0200

    ...

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main
     public static void main(String[] args) {
         //TerminalMenu.terminalMenu();
 
+        // Test
         ReadFile readFile = new ReadFile();
 
         readFile.readFile();
